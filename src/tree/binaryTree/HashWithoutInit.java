b1dd8c090e14164355d8592a736a5668227d6356
ShelleyRuirui <shelleyruirui@gmail.com>
2014-09-12 16:51:35 +0800

    Add more stack examples

---
+++ src/tree/binaryTree/HashWithoutInit.java
@@ -0,0 +1,36 @@
+package tree.binaryTree;
+
+//Problem 11.4 from "Introduction to algorithms"
+public class HashWithoutInit {
+
+	//Note that hash is very large and may not be initialized properly
+	public int search(int[] hash,int[] stack,int value){
+		int p=hash[value];
+		if(p>0 && p<=stack.length && stack[p]==value) return p;
+		return 0;
+	}
+	
+	public int insert(int[] hash,int[] stack,int value){
+		int exist=search(hash,stack,value);
+		if(exist!=0){
+			System.out.println("Already exist");
+			return 0;
+		}
+		stack[0]++;
+		stack[stack[0]]=value;
+		hash[value]=stack[0];
+		return stack[0];
+	}
+	
+	public int delete(int[] hash,int[] stack,int value){
+		int exist=search(hash,stack,value);
+		if(exist==0){
+			System.out.println("Not exist");
+			return 0;
+		}
+		stack[exist]=stack[stack[0]]; //Fill original blank with last element
+		hash[stack[exist]]=exist;
+		stack[0]--;
+		return exist;
+	}
+}
---
+++ src/tree/binaryTree/QueueCanReturnMax.java
@@ -0,0 +1,108 @@
+package tree.binaryTree;
+
+//A queue that is easy to locate max value
+public class QueueCanReturnMax {
+
+	private Stack input=new Stack(100);
+	private Stack output=new Stack(100);
+	
+	public int max(){
+		return Math.max(input.max(), output.max());
+	}
+	
+	public void enqueue(int value){
+		input.push(value);
+	}
+	
+	public int dequeue(){
+		if(output.size()>0) return output.pop();
+		while(input.size()>0){
+			output.push(input.pop());
+		}
+		return output.pop();
+	}
+	
+	public class Stack{
+		int[] stack;
+		int[] maxRelation;
+		int curMaxIndex;
+		int top;
+		int curMax;
+		
+		public Stack(int capacity){
+			stack=new int[capacity];
+			maxRelation=new int[capacity];
+			top=-1;
+			curMaxIndex=-1;
+			curMax=Integer.MIN_VALUE;
+		}
+		
+		public void push(int value){
+			//TODO: capacity check
+			top++;
+			stack[top]=value;
+			if(value>curMax){
+				maxRelation[top]=curMaxIndex;
+				curMaxIndex=top;
+				curMax=value;
+			}
+		}
+		
+		public int pop(){
+			//TODO: empty check
+			int result=stack[top];
+			
+			if(curMaxIndex==top){
+				curMaxIndex=maxRelation[top];
+				if(curMaxIndex!=-1)
+					curMax=stack[curMaxIndex];
+			}
+			
+			top--;
+			return result;
+		}
+		
+		public int max(){
+			//TODO: check empty
+			return curMax;
+		}
+		
+		public int size(){
+			return top+1;
+		}
+		
+	}
+	
+	public void testStack(){
+		Stack s=new Stack(100);
+		s.push(0);
+		s.push(10);
+		s.push(5);
+		s.push(20);
+		System.out.println(s.max());
+		s.pop();
+		System.out.println(s.max());
+		s.pop();
+		System.out.println(s.max());
+		s.pop();
+		System.out.println(s.max());
+	}
+	
+	public static void main(String[] args){
+		QueueCanReturnMax q=new QueueCanReturnMax();
+		q.enqueue(1);
+		q.enqueue(5);
+		q.enqueue(3);
+		q.enqueue(4);
+		
+//		q.testStack();
+		
+		System.out.println(q.max());
+		System.out.println(q.dequeue());
+		System.out.println(q.max());
+		System.out.println(q.dequeue());
+		System.out.println(q.max());
+		System.out.println(q.dequeue());
+		System.out.println(q.max());
+	}
+}
